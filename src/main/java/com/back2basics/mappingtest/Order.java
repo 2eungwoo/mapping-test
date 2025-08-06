336cb6b8d71f19da087efb311b42ff7bc8581fb2
swlee <swo98@ajou.ac.kr>
2025-08-07 00:06:07 +0900

    fix: h2 예약어인 order때문에 table name 명시, yml에서 sql script true로 변경

--- src/main/java/com/back2basics/mappingtest/Order.java
+++ src/main/java/com/back2basics/mappingtest/Order.java
@@ -5,10 +5,12 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
 import java.util.ArrayList;
 import java.util.List;
 
 @Entity
+@Table(name = "orders")
 public class Order {
     @Id @GeneratedValue
     private Long id;
--- src/main/java/com/back2basics/mappingtest/OrderItem.java
+++ src/main/java/com/back2basics/mappingtest/OrderItem.java
@@ -3,8 +3,10 @@ package com.back2basics.mappingtest;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 
 @Entity
+@Table(name = "order_items")
 public class OrderItem {
     @Id
     @GeneratedValue
