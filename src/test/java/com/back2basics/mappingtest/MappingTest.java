391e7812755ad9c84d801a08bcb390d1f437b8ff
swlee <swo98@ajou.ac.kr>
2025-08-07 00:21:43 +0900

    fix: getter 추가하고 테스트코드 수정

--- src/test/java/com/back2basics/mappingtest/MappingTest.java
+++ src/test/java/com/back2basics/mappingtest/MappingTest.java
@@ -17,12 +17,13 @@ class MappingTest {
     @Test
     void oneToManyTest() {
         Order order = new Order();
-        em.persist(order);
+        em.persist(order); // 1-side 먼저 저장
 
         OrderItem orderItem = new OrderItem();
-        // order.getItems().add(item); -- 단방향이지만 객체 컬렉션에 추가해줘야됨
+        order.getItems().add(orderItem); // -- 단방향이지만 객체 컬렉션에 추가해줘야됨
 
-        // 이렇게 하면 연관관계가 생기겠지만 쿼리 로그 보면 null로 들어감(order_id)
         em.persist(orderItem);
+
+        em.flush();
     }
 }
