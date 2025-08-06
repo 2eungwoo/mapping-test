4f9f83e522bc925505ad6f90d63e374983fd3fb3
swlee <swo98@ajou.ac.kr>
2025-08-06 23:59:03 +0900

    init

---
+++ src/main/java/com/back2basics/mappingtest/MappingtestApplication.java
@@ -0,0 +1,13 @@
+package com.back2basics.mappingtest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MappingtestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MappingtestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/back2basics/mappingtest/MappingtestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.back2basics.mappingtest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MappingtestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
